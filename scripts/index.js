f44ea27d1da9dbb27372000f087e086dc8813a43
jeffgtxjava <jeffgtxjava@gmail.com>
2021-01-18 22:05:33 +0530

    added allPosts and ceatePosts along with the functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,25 @@
 let allPosts = document.getElementById('allPosts-btn');
+let createPost = document.getElementById('createPost-btn');
+let createPostClose = document.getElementById('createPostClose');
+let createPostModal = document.getElementById('modal-createPost');
+
 
 allPosts.addEventListener('click',redirectToBlogList);
+createPost.addEventListener('click',displayCreatePostModal);
+createPostClose.addEventListener('click',closeCreatePostModal);
+
+
+
 
 function redirectToBlogList(){
     window.location.href='bloglist.html';
+}
+
+function closeCreatePostModal(){
+    createPostModal.style.display='none';
+}
+
+
+function displayCreatePostModal(){
+    createPostModal.style.display='block';
 }
